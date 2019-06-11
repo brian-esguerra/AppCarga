4752c87dd564cdd3c44dbf1e2c585bd51a9b359c
brian esguerra <brian-esguerra@upc.edu.co>
2019-06-11 17:05:56 -0500

    Add DB registros users

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -20,7 +20,7 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
           <View style={styles.welcomeContainer}>
           </View>
 
@@ -46,13 +46,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
   contentContainer: {
     paddingTop: 30,
   },
@@ -61,27 +54,9 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 20,
   },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
   getStartedContainer: {
     alignItems: 'center',
     marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
   },
   getStartedText: {
     fontSize: 17,
@@ -113,19 +88,5 @@ const styles = StyleSheet.create({
     fontSize: 17,
     color: 'rgba(96,100,109, 1)',
     textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
   },
 });
--- screens/RegistroScreen.js
+++ screens/RegistroScreen.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Platform, ScrollView, StyleSheet ,TextInput, View, TouchableHighlight, KeyboardAvoidingView} from 'react-native';
+import { Platform, ScrollView, StyleSheet ,TextInput, View,Alert, TouchableHighlight, KeyboardAvoidingView} from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 
 import { Button, Icon, Divider, Avatar,Text } from 'react-native-elements';
 import { createStackNavigator, createAppContainer } from 'react-navigation'; // Version can be specified in package.json
 
 import * as firebase from 'firebase';
-
+import ignoreWarnings from 'react-native-ignore-warnings';
 
 export default class RegistroScreen extends React.Component {
 
@@ -15,6 +15,11 @@ export default class RegistroScreen extends React.Component {
     this.state = {
       email:'',
       password:'',
+      name:'',
+      document:'',
+      phone:'',
+      plate:'',
+      typeAuto:'',
       response:''
     }
 
@@ -24,15 +29,29 @@ export default class RegistroScreen extends React.Component {
 
   async signUp() {
     try {
+      ignoreWarnings('Setting a timer');
       await firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
       
       this.setState({
         response: 'Usuario registrado correctamente'
       })
 
-      setTimeout(() => {
-        this.props.navigation.push('Navegacion')
-      },1500 )
+      //Registrar info en Bd
+      firebase.database().ref('Users/').push({
+          email: this.state.email,
+          name: this.state.name,
+          document: this.state.document,
+          phone: this.state.phone,
+          plate: this.state.plate,
+          typeAuto: this.state.typeAuto
+      }).then((data)=>{
+          //success callback
+          this.props.navigation.push('Navegacion')
+      }).catch((error)=>{
+          //error callback
+          console.log('error ' , error)
+          alert('Datos erroneos')
+      });
 
     }
     catch(error){
@@ -46,14 +65,14 @@ export default class RegistroScreen extends React.Component {
 
     return (
       <KeyboardAvoidingView behavior='padding' style={styles.container}>
-          <View style={styles.logoContainer}>  
+        <View style={styles.logoContainer}>  
+          <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
               <Text h3 style={styles.title}>
                 Crear cuenta
               </Text>
-
               <View style={styles.form}>
                 <Text style={styles.text_input}> 
-                  Correo electrónico </Text>
+                  Correo electrónico:</Text>
                 <TextInput
                   style={styles.input}
                   placeholder='username o email'
@@ -63,33 +82,87 @@ export default class RegistroScreen extends React.Component {
                   placeholderTextColor='rgba(255,255,255,0.7)'
                 />
                 <Text style={styles.text_input}>
-                  Clave </Text>
+                  Clave:</Text>
                 <TextInput
                   style={styles.input}
                   returnKeyType='next'
+                  maxLength={10}
                   onChangeText={(password) => this.setState({password})}
                   secureTextEntry
                 />
+
+                <Divider style={styles.divider}></Divider>
+
                 <Text style={styles.text_input}>
-                  Teléfono </Text>
+                  Nombre:</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder='nombre completo'
+                  returnKeyType='next'
+                  onChangeText={(name) => this.setState({name})}
+                  placeholderTextColor='rgba(255,255,255,0.7)'
+                />
+
+                <Text style={styles.text_input}> 
+                  No. Cédula:</Text>
+                <TextInput 
+                  style={styles.input}
+                  placeholder='no. cédula'
+                  returnKeyType='next'
+                  keyboardType='numeric'
+                  maxLength={12}
+                  onChangeText={(document) => this.setState({document})}
+                  placeholderTextColor='rgba(255,255,255,0.7)'
+                />
+
+                <Text style={styles.text_input}>
+                  Celular:</Text>
                 <TextInput
                   style={styles.input}
                   placeholder='teléfono o celular'
+                  returnKeyType='next'
                   keyboardType='numeric'
+                  maxLength={10}
+                  onChangeText={(phone) => this.setState({phone})}
                   placeholderTextColor='rgba(255,255,255,0.7)'
                 />
+
+                <Divider style={styles.divider}></Divider>
+
+                <Text style={styles.text_input}>
+                  Placa Vehículo:</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder='no. placa'
+                  returnKeyType='next'
+                  maxLength={6}
+                  onChangeText={(plate) => this.setState({plate})}
+                  placeholderTextColor='rgba(255,255,255,0.7)'
+                />
+
+                <Text style={styles.text_input}>
+                  Tipo de Vehículo:</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder='camioneta, turbo, sencillo, mula'
+                  maxLength={15}
+                  onChangeText={(typeAuto) => this.setState({typeAuto})}
+                  placeholderTextColor='rgba(255,255,255,0.7)'
+                />
+
               </View>
           
               <View style={styles.div}>
                 <TouchableHighlight style={styles.btn_login} onPress={this.signUp}>
-                  <Text style={styles.text_btn}> Continuar </Text>
+                  <Text style={styles.text_btn_login}> Continuar </Text>
                 </TouchableHighlight>
               </View>
               <View style={styles.div}>
                 <TouchableHighlight style={styles.btn_reg} onPress={() => this.props.navigation.goBack()}>
-                  <Text style={styles.text_btn}> Regresar </Text>
+                  <Text style={styles.text_btn_reg}> Regresar </Text>
                 </TouchableHighlight>
               </View>
+            </ScrollView>
           </View>
         </KeyboardAvoidingView>
     );
@@ -110,10 +183,13 @@ const styles = StyleSheet.create({
     flexGrow:1,
     justifyContent:'center'
   },
+  contentContainer:{
+    marginTop:10
+  },
   title:{
     alignItems:'center',
     marginTop: 10,
-    marginBottom:20,
+    marginBottom:10,
     color: '#40739e',
     opacity:0.9,
     textAlign: 'center'
@@ -145,6 +221,12 @@ const styles = StyleSheet.create({
     paddingLeft:10,
     paddingRight:10
   },
+  divider:{
+    height:2,
+    backgroundColor: '#DDDDDD',
+    marginTop:10,
+    marginBottom:10
+  },
   //btn login
   btn_login:{
     alignItems: 'center',
@@ -159,7 +241,12 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius:8
   },
-  text_btn:{
-    fontSize: 16
+  text_btn_login:{
+    fontSize: 16,
+    color: '#DDDDDD'
+  },
+  text_btn_reg:{
+    fontSize: 16,
+    color: '#95afc0'
   }
 });
